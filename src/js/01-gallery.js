e8a68c63b7e349d47983b424c81d5d74a391bd91
Olena Shavala <olena.shvl@gmail.com>
2023-05-28 10:09:44 +0300

    first fixation

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+const gallery = document.querySelector('.gallery');
+
+
+
+const GalleryMarkup = image => {
+  const { preview, original, description } = image;
+
+  return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+};
+
+const makeGallery = galleryItems.map(GalleryMarkup).join('');
+
+
+gallery.insertAdjacentHTML('afterbegin', makeGallery);
+
+
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+
+
 console.log(galleryItems);
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
 
+import Player from '@vimeo/player';
+
+import  throttle  from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe, {
+  loop: true,
+  fullscreen: true,
+  quality: '1080p',
+});
+
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+player.on(
+  'timeupdate',
+  throttle(e => {
+    localStorage.setItem(LOCALSTORAGE_KEY, e.seconds); 
+  }, 1000)
+);
+
+const currentTime = localStorage.getItem(LOCALSTORAGE_KEY);
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
+
+
